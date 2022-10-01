c89f3fad13624504f924387943841e65c17e1ed1
Jeffylison <100355251+Jeffylison@users.noreply.github.com>
2022-10-01 21:56:17 +0800

    6

--- ruige/源4.cpp
+++ ruige/源4.cpp
@@ -1,15 +1,38 @@
 #include<stdio.h>
+/*int main()
+{
+	int a, b,c=0,sum = 0;
+	scanf_s("%d", &a);
+	while (a!= EOF)
+	{
+		for (b = 2; b <= a; b++)
+		{
+			for (c = 1; c <= b; c++)
+				if (b % c == 0 && (c == 1 || b))
+					sum += c - 1;
+			
+		}
+	}
+	printf("%d", sum);
+	return 0;
+}*/
 int main()
 {
-	int a, b, sum = 0;
-	while (scanf_s("%d", &a) != EOF)
+	int a = 0 , b = 0, c,sum=0;
+	while (scanf("%d", &c) != EOF)
 	{
-		for (b = 0; b <= a; b++)
-			if (a % b == 0)
+		sum = 0;
+		for (a = 2; a <= c; a++)
+		{
+			for (b = 2; b <= a; b++)
 			{
-				printf("%d", b);
-				printf("%d", sum += b);
+				if (a % b == 0)
+					break;
 			}
+			if (a == b)
+				sum += b;
+		}
+		printf("%d\n", sum);
 	}
 	return 0;
-}
+}//总算做对了哈哈哈哈哈哈哈（求多组素数之和）
