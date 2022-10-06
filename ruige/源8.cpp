454de7198f615d2b6c890d0cf1d9446db122195e
Jeffylison <100355251+Jeffylison@users.noreply.github.com>
2022-10-06 12:45:13 +0800

    6

---
+++ ruige/源8.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a[20], i;
+	a[20] = 0;
+	a[0] = 0; a[1] = 0; a[2] = 1;
+	printf("%d %d %d ", a[0], a[1], a[2]);
+	for (i = 3; i < 20; i++)
+	{
+		a[i] = a[i - 1] + a[i - 2] + a[i - 3];
+		printf("%d ", a[i]);
+	}
+	return 0;
+}
