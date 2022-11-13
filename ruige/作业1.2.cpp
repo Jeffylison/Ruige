1917dc807c4c8e1976e28a903809a05f253f2649
Jeffylison <100355251+Jeffylison@users.noreply.github.com>
2022-11-13 20:38:08 +0800

    Create 作业1.2.cpp

---
+++ ruige/作业1.2.cpp
@@ -0,0 +1,13 @@
+//ZY1.2
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int F;
+    double C;
+    scanf("%d",&F);
+    C=5.00*(F-32.00)/9.00;
+    printf("%.2lf",C);
+    return 0;
+}
