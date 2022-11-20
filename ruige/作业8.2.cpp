c79de5cb71124d732a7a56b6283b3385fa9a08ba
Jeffylison <100355251+Jeffylison@users.noreply.github.com>
2022-11-20 17:04:27 +0800

    Update 作业8.2.cpp

--- ruige/作业8.2.cpp
+++ ruige/作业8.2.cpp
@@ -1,32 +1,34 @@
 //ZY8.2
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 int main()
 {
     char a[100];
     int i,j,n,t,m;
-    while(gets(a)!=EOF)
+    while(gets(a)!=NULL)
     {
-        t=0;m=0;
-        n=strlen(a);
-        for(i=0;i<n;i++)
+        t=0;
+        for(i=0;a[i]!='\0'; i++)
         {
             if((a[i]>='a'&&a[i]<='z')||(a[i]>='A'&&a[i]<='Z'))
-               if(!(a[i+1]>='a'&&a[i+1]<='z')&&!(a[i+1]>='A'&&a[i+1]<='Z'))
-                t++;
+                if(!(a[i+1]>='a'&&a[i+1]<='z')&&!(a[i+1]>='A'&&a[i+1]<='Z'))
+                    t++;
         }
         printf("%d\n",t);
-        for(i=0;i<n;i++)
+        for(i=0;a[i]!='\0'; i++)
         {
             if((a[i]>='a'&&a[i]<='z')||(a[i]>='A'&&a[i]<='Z'))
-               if(!(a[i+1]>='a'&&a[i+1]<='z')&&!(a[i+1]>='A'&&a[i+1]<='Z'))
-               {for(j=i;j>=m;j--)
-                    printf("%c",a[j]);
+                if(!(a[i+1]>='a'&&a[i+1]<='z')&&!(a[i+1]>='A'&&a[i+1]<='Z'))
+                {
+                    for(j=i; j>=0; j--)
+                    {
+                        if(a[j]==' ') break;
+                        printf("%c",a[j]);
+                    }
                     printf(" ");
-                    m=i+2;}
+                }
         }
-        //printf("\n");
+        printf("\n");
     }
     return 0;
 }
