3da4b47f288db3312311880958b87aa62ddf0c5e
Jeffylison <100355251+Jeffylison@users.noreply.github.com>
2022-11-20 16:25:15 +0800

    OK

--- ruige/作业10.1.cpp
+++ ruige/作业10.1.cpp
@@ -8,45 +8,62 @@ typedef struct node
 }NODE;
 int main()
 {
-    NODE *phead,*ptail,*pnew,*p;
-    int i,a,n,m;
+    NODE *phead,*ptail,*pnew;
+    int i,n,a,m;
     while(scanf("%d",&n)!=EOF)
     {
         phead=(NODE *)malloc(sizeof(NODE));
         if(phead==NULL) exit(1);
-        p=(NODE *)malloc(sizeof(NODE));
         phead->next=NULL;
         ptail=phead;
         for(i=0;i<n;i++)
         {
-            scanf("%d",&a);
             pnew=(NODE *)malloc(sizeof(NODE));
+            if(pnew==NULL) exit(1);
+            scanf("%d",&a);
             pnew->data=a;
             pnew->next=NULL;
             ptail->next=pnew;
             ptail=pnew;
         }
-        scanf("%d",&m);
-        ptail=phead;
-        while(ptail)
+        /*phead=phead->next;
+        while(phead)
         {
-            p=ptail;
-            ptail=ptail->next;
-            if(ptail->data>m)
+            printf("%d ",phead->data);
+            phead=phead->next;
+        }
+        printf("\n");*/
+        scanf("%d",&m);
+        NODE *p,*q;
+        q=phead;
+        p=phead->next;
+        while(1)
+        {
+            if(p->data>m)
             {
                 pnew=(NODE *)malloc(sizeof(NODE));
                 pnew->data=m;
-                pnew->next=p->next;
+                pnew->next=p;
+                q->next=pnew;
+                break;
+            }
+            else if(p->next==NULL)
+            {
+                pnew=(NODE *)malloc(sizeof(NODE));
+                pnew->data=m;
+                pnew->next=NULL;
                 p->next=pnew;
                 break;
             }
+             else {q=p;p=p->next;}
         }
         phead=phead->next;
         while(phead)
-            {
-                printf("%d  ",phead->data);
-                phead=phead->next;
-            }
+        {
+            printf("%d  ",phead->data);
+            phead=phead->next;
+        }
+        printf("\n");
     }
     return 0;
 }
