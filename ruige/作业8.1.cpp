c83f09b40f606429bef3d41d22df5dbddff36833
Jeffylison <100355251+Jeffylison@users.noreply.github.com>
2022-11-20 16:48:26 +0800

    OK

--- ruige/作业8.1.cpp
+++ ruige/作业8.1.cpp
@@ -5,37 +5,29 @@
 int main()
 {
     int i,n,j,b[100];
-    char a[10][100];
-    while(scanf("%d",&n)!=EOF)
+    char a[100][100];
+    while(scanf("%d\n",&n)!=EOF)
     {
-        for(i=0;i<n;i++)
-            b[i]=1;
-        for(i=0;i<n;i++)
-            gets(a[i]);
-        for(i=0;i<n;i++)
+        for(i=0; i<n; i++)
         {
-            for(j=i-1;j>=0;j--)
-                if(strcmp(a[j],a[i])==0)
-                    break;
-            if(strcmp(a[j],a[i])==0)
-               break;
-            //printf("%s\n",a[i]);
-            for(j=i+1;j<n;j++)
+            b[i]=1;
+            gets(a[i]);
+        }
+        for(i=0; i<n; i++)
+        {
+            for(j=i+1; j<n; j++)
                 if(strcmp(a[i],a[j])==0)
                     b[i]++;
-        }
-        for(i=0;i<n;i++)
-        {
-            for(j=i-1;j>=0;j--)
+            for(j=i-1; j>=0; j--)
                 if(strcmp(a[j],a[i])==0)
                     break;
             if(strcmp(a[j],a[i])==0)
-                {i++;continue;}
+                continue;
             //printf("%s\n",a[i]);
             printf("%d ",b[i]);
             printf("%s\n",a[i]);
-        }
 
+        }
     }
     return 0;
 }
