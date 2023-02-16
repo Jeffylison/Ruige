2e32d459de2141a0b5eb80f946cc2dbd21432e38
Jeffylison <100355251+Jeffylison@users.noreply.github.com>
2023-02-16 18:26:19 +0800

    8975

---
+++ ruige-RST/实验5.1.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main()
+{
+    int a[20],n,i,m,j;
+    while(scanf("%d%d",&n,&m)!=EOF)
+    {
+        for(i=1; i<=n; i++)
+            scanf("%d",&a[i]);
+        for(i=1; i<=n; i++)
+        {
+            if(a[i]<m)//5 4  3  2 1
+            {
+                for(j=n+1;j>i;j--)
+                    a[j]=a[j-1];
+                a[i]=m;
+                break;
+            }
+        }
+        for(i=1; i<=n+1; i++)
+            printf("%d ",a[i]);
+    }
+    return 0;
+}
